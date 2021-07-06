aeb81b0aba19c956364ff9f73ef76f5df040de70
Kim Eunho <thinkingim@gmail.com>
2021-07-06 05:42:55 +0000

    restructure template project

--- pipeline_stack/app_stage.py
+++
@@ -1,9 +0,0 @@
-from aws_cdk import core
-
-from app_stack.my_service_stack import MyServiceStack
-
-class AppStage(core.Stage):
-    def __init__(self, scope: core.Construct, id: str, **kwargs):
-        super().__init__(scope, id, **kwargs)
-
-        MyServiceStack(self, 'MyServiceStack')
