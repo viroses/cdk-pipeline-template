91860f2109a1ab1ad908ba1ab7f50d1a17c55479
viroses <viros1984@gmail.com>
2021-07-29 13:57:00 +0000

    creating ecs sample task definition and service

--- artifacts/app/__init__.py
+++ artifacts/app/__init__.py
@@ -10,3 +10,32 @@ class Application(core.NestedStack):
         super().__init__(scope, id, **kwargs)
         
         cluster = ecs.Cluster(self,"ObservationCluster", vpc=bmt_vpc)
+        
+        # Add capacity to it
+        cluster.add_capacity("DefaultAutoScalingGroupCapacity",
+            instance_type=ec2.InstanceType("t2.xlarge"),
+            desired_capacity=3
+        )
+        
+        task_definition = ecs.Ec2TaskDefinition(self, "TaskDef")
+        
+        task_definition.add_container("DefaultContainer",
+            image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample"),
+            memory_limit_mi_b=512
+        )
+        
+        # Instantiate an Amazon ECS Service
+        ecs_service = ecs.Ec2Service(self, "Service",
+            cluster=cluster,
+            task_definition=task_definition
+        )
+        
+        # fargate_task_definition = ecs.FargateTaskDefinition(self, "TaskDef",
+        #     memory_limit_mi_b=512,
+        #     cpu=256
+        # )
+        # # Example automatically generated without compilation. See https://github.com/aws/jsii/issues/826
+        # container = fargate_task_definition.add_container("WebContainer",
+        #     # Use an image from DockerHub
+        #     image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample")
+        # )
