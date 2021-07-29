e05a5eaebb8ac735bcf1651ea90cb94fa2f8c625
viroses <viros1984@gmail.com>
2021-07-29 16:28:23 +0000

    remove ecs cluster

--- artifacts/app/__init__.py
+++ artifacts/app/__init__.py
@@ -10,41 +10,42 @@ class Application(core.NestedStack):
     def __init__(self, scope: core.Construct, id: str, bmt_vpc: ec2.Vpc, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
         
-        cluster = ecs.Cluster(self,"ObservationCluster", 
-            enable_fargate_capacity_providers=True,
-            vpc=bmt_vpc
-        )
+        # cluster = ecs.Cluster(self,"ObservationCluster", 
+        #     enable_fargate_capacity_providers=True,
+        #     vpc=bmt_vpc
+        # )
 
-        fargate_task_definition = ecs.FargateTaskDefinition(self, "TaskDef",
-            memory_limit_mib=512,
-            cpu=256
-        )
-        
-        # log_container = fargate_task_definition.add_firelens_log_router("LogContainer",
-        #     firelens_config=ecs.FirelensConfig(
-        #         type=ecs.FirelensLogRouterType.FLUENTBIT
-        #     ),
-        #     image=ecs.ContainerImage.from_ecr_repository(ecr.IRepository.repository_uri_for_tag("906394416424.dkr.ecr.ap-northeast-2.amazonaws.com/aws-for-fluent-bit")),
-        #     logging=ecs.LogDrivers.aws_logs(stream_prefix="observation-log-stream")
+        # fargate_task_definition = ecs.FargateTaskDefinition(self, "TaskDef",
+        #     memory_limit_mib=512,
+        #     cpu=256
         # )
         
-        web_container = fargate_task_definition.add_container("WebContainer",
-            # Use an image from DockerHub
-            image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample")
-            # logging=ecs.LogDrivers.firelens(
-            #     options={
-            #         "Name": "cloudwatch",
-            #         "log_group_name": "observation-log",
-            #         "region": "ap-northeast-2"
-            #     }
-    # )
-        )
+        # # log_container = fargate_task_definition.add_firelens_log_router("LogContainer",
+        # #     firelens_config=ecs.FirelensConfig(
+        # #         type=ecs.FirelensLogRouterType.FLUENTBIT
+        # #     ),
+        # #     image=ecs.ContainerImage.from_ecr_repository(ecr.IRepository.repository_uri_for_tag("906394416424.dkr.ecr.ap-northeast-2.amazonaws.com/aws-for-fluent-bit")),
+        # #     logging=ecs.LogDrivers.aws_logs(stream_prefix="observation-log-stream")
+        # # )
+        
+        # fargate_task_definition.add_container("WebContainer",
+        #     # Use an image from DockerHub
+        #     image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample"),
+        #     port_mappings=ecs.PortMapping(80,0),
+        #     logging=ecs.LogDrivers.firelens(
+        #         options={
+        #             "Name": "cloudwatch",
+        #             "log_group_name": "observation-log",
+        #             "region": "ap-northeast-2"
+        #         }
+        #     )
+        # )
 
         
         
-        # Instantiate an Amazon ECS Service
-        ecs_service = ecs.FargateService(self, "Service",
-            cluster=cluster,
-            task_definition=fargate_task_definition,
-            desired_count=3
-        )
+        # # Instantiate an Amazon ECS Service
+        # ecs_service = ecs.FargateService(self, "Service",
+        #     cluster=cluster,
+        #     task_definition=fargate_task_definition,
+        #     desired_count=3
+        # )
