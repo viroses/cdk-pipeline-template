3ef2f9da02f461944d8bd2420e4b42b3219f3ded
viroses <viros1984@gmail.com>
2021-07-30 02:22:21 +0000

    ecs task def

--- artifacts/app/__init__.py
+++ artifacts/app/__init__.py
@@ -10,37 +10,31 @@ class Application(core.NestedStack):
     def __init__(self, scope: core.Construct, id: str, bmt_vpc: ec2.Vpc, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
         
-        # cluster = ecs.Cluster(self,"ObservationCluster", 
+        # cluster = ecs.Cluster(self,"ObservationCluster",
         #     enable_fargate_capacity_providers=True,
         #     vpc=bmt_vpc
         # )
-
+        
         fargate_task_definition = ecs.FargateTaskDefinition(self, "TaskDef",
             memory_limit_mib=512,
             cpu=1024
         )
         
-        fargate_task_definition.add_container("WebContainer",
+        web_container = fargate_task_definition.add_container("WebContainer",
             image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample"),
-            # port_mappings=ecs.PortMapping([80],[0]),
-            logging=ecs.LogDrivers.firelens(
-                options={
-                    "Name": "cloudwatch",
-                    "log_group_name": "observation-log",
-                    "region": "ap-northeast-2"
-                }
-            ),
-            memory_limit_mib=128,
-            cpu=0
+            # port_mappings=ecs.PortMapping[{"container_port": 80},{"container_port": 80}],
+            # logging=ecs.LogDrivers.aws_logs(stream_prefix="observation-log-stream"),
+            memory_limit_mib=512,
+            cpu=256
         )
         
-        fargate_task_definition.add_firelens_log_router("LogContainer",
-            firelens_config=ecs.FirelensConfig(
-                type=ecs.FirelensLogRouterType.FLUENTBIT
-            ),
-            image=ecs.ContainerImage.from_registry("906394416424.dkr.ecr.ap-northeast-2.amazonaws.com/aws-for-fluent-bit"),
-            logging=ecs.LogDrivers.aws_logs(stream_prefix="observation-log-stream")
-        )
+        # fargate_task_definition.add_firelens_log_router("LogContainer",
+        #     firelens_config=ecs.FirelensConfig(
+        #         type=ecs.FirelensLogRouterType.FLUENTBIT
+        #     ),
+        #     image=ecs.ContainerImage.from_registry("906394416424.dkr.ecr.ap-northeast-2.amazonaws.com/aws-for-fluent-bit"),
+        #     logging=ecs.LogDrivers.aws_logs(stream_prefix="observation-log-stream")
+        # )
 
         # # Instantiate an Amazon ECS Service
         # ecs_service = ecs.FargateService(self, "Service",
